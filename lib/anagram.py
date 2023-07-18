14d0bcc49f7e109a0c53df9a11769e78a3414b90
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-07-17 21:37:39 -0500

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+
+class Anagram:
+    new_list = []
+    def __init__(self, word = ""):
+        self.word = word
+    
+    def match(self, words_list):
+        return [(item) for item in words_list if sorted(item) == sorted(self.word)]
+        
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
